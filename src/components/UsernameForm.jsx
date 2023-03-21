da837cf7cfca2b8fec9b331989cbcbdde75dc094
Son Dao <udemydaohsn@gmail.com>
2023-03-21 10:03:14 -0700

    update UsernameForm

--- src/components/UsernameForm.jsx
+++ src/components/UsernameForm.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import useUserInfo from "@/hooks/useUserInfo";
+import React, { useEffect, useState } from "react";
 
 const UsernameForm = () => {
+  const { userInfo, userInfoStatus } = useUserInfo();
   const [username, setUserName] = useState("");
+
+  useEffect(() => {
+    if (userInfoStatus === "loading") {
+      return;
+    }
+    if (username === "") {
+      const defaultUserName = userInfo?.email.split("@")[0];
+      setUserName(defaultUserName);
+    }
+  }, [userInfoStatus]);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
     console.log(username);
   };
+
   return (
     <div className="flex h-screen items-center justify-center">
       <form className="text-center" onSubmit={handleFormSubmit}>
@@ -14,7 +27,9 @@ const UsernameForm = () => {
         <input
           className="block mb-1 bg-twitterBorder px-3 py-1 rounded-full"
           type="text"
-          placeholder="username"
+          placeholder={
+            userInfo?.email.split("@")[0].replace(/[^a-z]+/gi, "") || "username"
+          }
           value={username}
           onChange={(e) => {
             setUserName(e.target.value);
--- src/hooks/useUserInfo.js
+++ src/hooks/useUserInfo.js
@@ -17,7 +17,6 @@ const useUserInfo = () => {
       });
     });
   }
-
   useEffect(() => {
     getUserInfo();
   }, [status]);
